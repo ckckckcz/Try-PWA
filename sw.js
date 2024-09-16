aa36f7a1076f02663ecf1aba83416e784baf4f75
ckckckcz <rriovld@gmail.com>
2024-09-16 08:52:09 +0700

    initial

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "pwa-cache-v1";
+const urlsToCache = ["/", "/index.html", "/style.css"];
+
+// Install service worker dan cache file
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch dan berikan file yang di-cache saat offline
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
